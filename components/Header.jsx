ef0c00a5d6462852d37651f65e95023a5698ab3b
mfahadjbr <mfahadjbr@gmail.com>
2024-10-26 01:18:30 +0500

    fahad

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function Header() {
   return (
     <>
-      <header className="bg-slate-300 text-gray-600 body-font">
+      <header className="bg-slate-300 text-gray-600 body-font ">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           {/* Logo Section */}
           <Link href={'./'} className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
